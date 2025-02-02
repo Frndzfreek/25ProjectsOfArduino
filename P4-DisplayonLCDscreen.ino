ebfd4855f1987189767217d772e50e7b2438e1eb
Madhumitha <madhu.mithaemails@gmail.com>
2025-02-02 16:42:21 +0530

    Update and rename P4-DisplayonLCDscreen to P4-DisplayonLCDscreen.ino

--- P4-DisplayonLCDscreen.ino
+++ P4-DisplayonLCDscreen.ino
@@ -1,14 +1,17 @@
 //Here in this project on how to display texts on lcd screen
 //including the liquidcrystal library
+
 #include<LiquidCrystal.h>
 LiquidCrystal lcd(7,6,5,4,3,2);
 String text="Hello World!"; //initializing the text to be displayed
+
 //setting up the void setup()
 void setup(){
   lcd.begin(16,2);
   lcd.clear();
   lcd.print(text);
 }
+
 //void loop() function is not required
 void loop() {
 }
